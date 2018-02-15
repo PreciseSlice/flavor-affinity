e601c5710095672951a2d10875d46cc1a5109a8d
Spencer Herms <slherms@gmail.com>
2018-02-14 21:38:23 -0700

    add submit form method and MDTP

--- src/Components/MainForm/MainForm.js
+++ src/Components/MainForm/MainForm.js
@@ -1,33 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './MainForm.css';
+import { connect } from 'react-redux';
+//import * as actions from '../../Actions';
+import { setIngredients } from '../../Actions/index'
+import { searchForIngredient } from '../../Helpers/apiCalls';
 
 class MainForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userSearch: ''
-    } 
+      userInput: ''
+    };
   }
 
   handleChange(event) {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     this.setState({ [name]: value });
+  }
+
+  submitForm(event) {
+    const { setIngredients } = this.props;
+    event.preventDefault();
+    const { userInput } = this.state;
+    const searchReasult = searchForIngredient(userInput);
+    setIngredients(searchReasult);
   }
 
   render() {
     return (
       <div className="form-container">
-        <form>
-          <input 
-            name="userSearch" 
-            type="text" 
-            onChange={ event => this.handleChange(event) } 
-            placeholder="enter ingredient" />
-          {/* <buttton>img</buttton> */}
+        <form onSubmit={event => this.submitForm(event)}>
+          <input
+            name="userInput"
+            type="text"
+            onChange={event => this.handleChange(event)}
+            placeholder="enter ingredient"
+          />
+          {/* <buttton>icon</buttton> */}
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default MainForm;
+// export const mapStateToProps = state => ({
+  
+// });
+
+export const mapDispatchToProps = dispatch => ({
+  setIngredients : ingredient =>
+    dispatch(searchForIngredient(ingredient))
+});
+
+export default connect(null, mapDispatchToProps)(MainForm);
