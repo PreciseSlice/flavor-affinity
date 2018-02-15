7e996ffabb4cc3e451e2c8bb92b21c390640a4b1
Spencer Herms <slherms@gmail.com>
2018-02-14 21:36:33 -0700

    add Ingredient reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+const ingredientsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'SET_INGREDIENT':
+      console.log('SET_INGREDIENT', action);
+      return state  
+    default:
+      return state
+  }
+}
 
+const rootReducer = combineReducers({
+  ingredients: ingredientsReducer
 });
 
 export default rootReducer;
