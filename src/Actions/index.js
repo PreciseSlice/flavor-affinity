4f8cd67e401ad091c886e4fa45c953507fb0e7ee
Spencer Herms <slherms@gmail.com>
2018-02-14 21:34:06 -0700

    add set ingredient action creator

---
+++ src/Actions/index.js
@@ -0,0 +1,22 @@
+//import { fetchFromApi } from '../Helpers/apiCalls';
+
+// export const searchForIngredient = async ingredient => {
+//     const searchResult = await fetchFromApi(
+//       `https://api.foodpairing.com/ingredients/?q=${ingredient}`
+//     );
+//     console.log(searchResult);
+
+//     return { type: 'SET_INGREDIENT', SET_INGREDIENT: searchResult };
+// };
+
+export const  setIngredients = ingredients => {
+  type: 'SET_INGREDIENT', 
+  ingredients
+}
+
+// export const  setIngredients = ingredients => {
+//   return dispatch => {
+//     type: 'SET_INGREDIENTS', 
+//     ingredients
+//   }
+// }
