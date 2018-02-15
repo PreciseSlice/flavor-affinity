9f948aded9e9fc27ce9a98204814634387fd61de
Spencer Herms <slherms@gmail.com>
2018-02-15 09:39:18 -0700

    takes out redux dev tools with incorrect argument

--- src/store.js
+++ src/store.js
@@ -2,9 +2,4 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './Reducers/index';
 import thunk from 'redux-thunk';
 
-export default () =>
-  createStore(
-    rootReducer,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+export default () => createStore(rootReducer, applyMiddleware(thunk));
