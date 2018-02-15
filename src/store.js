f9aef19c0222c37502e48183a44a9014f7bb541b
Spencer Herms <slherms@gmail.com>
2018-02-14 21:33:43 -0700

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,4 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './Reducers/index';
 import thunk from 'redux-thunk';
 
-export default () => createStore(rootReducer, applyMiddleware(thunk));
+export default () =>
+  createStore(
+    rootReducer,
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
