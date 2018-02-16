5fd1a3c1b9afb22556fbbb38e8568e531739beda
Spencer Herms <slherms@gmail.com>
2018-02-16 16:23:48 -0700

    add snapshot test for app

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,19 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('Exist and matches snapshot', () => {
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
