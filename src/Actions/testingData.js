75fbe544924bb339bcda077cae15e276bdd70f81
Spencer Herms <slherms@gmail.com>
2018-02-16 16:22:22 -0700

    adds mock data for testing

---
+++ src/Actions/testingData.js
@@ -0,0 +1,12 @@
+export const ingredients = [
+  {
+    id: 94,
+    name: 'Apple (fresh)',
+    preparation: 'fresh'
+  },
+  {
+    id: 6176,
+    name: 'Artichoke (cooked)',
+    preparation: 'cooked'
+  }
+];
