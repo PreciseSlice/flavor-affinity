a219ebfe9a2d0b5da3481dbe3e0ab9b5a13b9ed8
Spencer Herms <slherms@gmail.com>
2018-02-17 17:46:40 -0700

    add mockData for testing

---
+++ src/Components/App/testData.js
@@ -0,0 +1,173 @@
+/* eslint-disable */
+export const allIngredients = [
+  {
+    id: 5902,
+    name: '1724 Tonic',
+    product: '1724 Tonic',
+    preparation: '',
+    _links: {
+      preparation: null,
+      origin: null,
+      image: {
+        id: 4578,
+        src: 'https://pantry.foodpairing.com/apps/4578',
+        size_240: 'https://pantry.foodpairing.com/apps/4578_240_0',
+        size_480: 'https://pantry.foodpairing.com/apps/4578_480_0',
+        size_720: 'https://pantry.foodpairing.com/apps/4578_720_0',
+        size_960: 'https://pantry.foodpairing.com/apps/4578_960_0',
+        size_1200: 'https://pantry.foodpairing.com/apps/4578_1200_0',
+        size_1440: 'https://pantry.foodpairing.com/apps/4578_1440_0'
+      },
+      brand: null,
+      categories: [
+        {
+          id: 38,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/38'
+        },
+        {
+          id: 139,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/139'
+        }
+      ]
+    },
+    description:
+      'The essence of 1724 Tonic Water has its origins in the Andes, on the mythical Inca Trail. This is not by chance. This is where quinine was discovered. There, this ingredient gains the authenticity of a product with roots in the region that go back hundreds of years. At 1724 metres above sea level. Not a metre above or below.',
+    _meta: {
+      created_at: '',
+      modified_at: '',
+      index: 0
+    }
+  },
+  {
+    id: 5440,
+    name: 'A la Chimay Bleue',
+    product: 'A la Chimay Bleue',
+    preparation: '',
+    _links: {
+      preparation: null,
+      origin: null,
+      image: {
+        id: 1772,
+        src: 'https://pantry.foodpairing.com/apps/1772',
+        size_240: 'https://pantry.foodpairing.com/apps/1772_240_0',
+        size_480: 'https://pantry.foodpairing.com/apps/1772_480_0',
+        size_720: 'https://pantry.foodpairing.com/apps/1772_720_0',
+        size_960: 'https://pantry.foodpairing.com/apps/1772_960_0',
+        size_1200: 'https://pantry.foodpairing.com/apps/1772_1200_0',
+        size_1440: 'https://pantry.foodpairing.com/apps/1772_1440_0'
+      },
+      brand: {
+        id: 80,
+        name: 'Chimay',
+        website: '',
+        description: '',
+        _links: {
+          images: [
+            {
+              id: 4420,
+              src: 'https://pantry.foodpairing.com/apps/4420'
+            }
+          ]
+        }
+      },
+      categories: [
+        {
+          id: 265,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/265'
+        },
+        {
+          id: 266,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/266'
+        }
+      ]
+    },
+    description:
+      'The rind of the à la Chimay Bleue is regularly washed with the Chimay Blue Cap during his stay in the aging cellars of the monestarey. Pronounced flavor with a slight bitterness of the hops.',
+    _meta: {
+      created_at: '',
+      modified_at: '',
+      index: 1
+    }
+  },
+  {
+    id: 5443,
+    name: 'A la Chimay Rouge',
+    product: 'A la Chimay Rouge',
+    preparation: '',
+    _links: {
+      preparation: null,
+      origin: null,
+      image: {
+        id: 1773,
+        src: 'https://pantry.foodpairing.com/apps/1773',
+        size_240: 'https://pantry.foodpairing.com/apps/1773_240_0',
+        size_480: 'https://pantry.foodpairing.com/apps/1773_480_0',
+        size_720: 'https://pantry.foodpairing.com/apps/1773_720_0',
+        size_960: 'https://pantry.foodpairing.com/apps/1773_960_0',
+        size_1200: 'https://pantry.foodpairing.com/apps/1773_1200_0',
+        size_1440: 'https://pantry.foodpairing.com/apps/1773_1440_0'
+      },
+      brand: {
+        id: 80,
+        name: 'Chimay',
+        website: '',
+        description: '',
+        _links: {
+          images: [
+            {
+              id: 4420,
+              src: 'https://pantry.foodpairing.com/apps/4420'
+            }
+          ]
+        }
+      },
+      categories: [
+        {
+          id: 265,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/265'
+        },
+        {
+          id: 266,
+          href:
+            'https://api.foodpairing.com/taxonomies/ingredients/categories/266'
+        }
+      ]
+    },
+    description:
+      'The à la Chimay Rouge is a round, smooth cheese with creamy texture. Its natural rind is washed with Chimay Red Cap. Fruity flavour with slight bitterness.',
+    _meta: {
+      created_at: '',
+      modified_at: '',
+      index: 2
+    }
+  }
+];
+
+export const cleanData = [
+  {
+    description:
+      'The essence of 1724 Tonic Water has its origins in the Andes, on the mythical Inca Trail. This is not by chance. This is where quinine was discovered. There, this ingredient gains the authenticity of a product with roots in the region that go back hundreds of years. At 1724 metres above sea level. Not a metre above or below.',
+    id: 5902,
+    image: 'https://pantry.foodpairing.com/apps/4578_240_0',
+    name: '1724 Tonic'
+  },
+  {
+    description:
+      'The rind of the à la Chimay Bleue is regularly washed with the Chimay Blue Cap during his stay in the aging cellars of the monestarey. Pronounced flavor with a slight bitterness of the hops.',
+    id: 5440,
+    image: 'https://pantry.foodpairing.com/apps/1772_240_0',
+    name: 'A la Chimay Bleue'
+  },
+  {
+    description:
+      'The à la Chimay Rouge is a round, smooth cheese with creamy texture. Its natural rind is washed with Chimay Red Cap. Fruity flavour with slight bitterness.',
+    id: 5443,
+    image: 'https://pantry.foodpairing.com/apps/1773_240_0',
+    name: 'A la Chimay Rouge'
+  }
+];
