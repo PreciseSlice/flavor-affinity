3c3579409d00d84eb5c416600739c755389b7fbc
Spencer Herms <slherms@gmail.com>
2018-02-19 17:56:14 -0700

    add prefix trie autocomplete, suggestion datalist dropdown on input

--- src/Components/MainForm/MainForm.js
+++ src/Components/MainForm/MainForm.js
@@ -2,41 +2,83 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //import * as actions from '../../Actions';
 import { setIngredients } from '../../Actions/index';
-import { searchForIngredient } from '../../Helpers/apiCalls';
+//import { searchForIngredient } from '../../Helpers/apiCalls';
+import { PrefixTrie } from '@PreciseSlice/complete-me';
 import './MainForm.css';
+//import PropTypes from 'prop-types';
 
 export class MainForm extends Component {
   constructor(props) {
     super(props);
+
+    this.trie = new PrefixTrie();
+
     this.state = {
-      userInput: ''
+      userInput: '',
+      allIngredients: [],
+      suggestedIngredients: []
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.ingredientsObject !== nextProps.ingredientsObject) {
+      const { ingredientsObject } = nextProps;
+      const nameArray = ingredientsObject.ingredients.map(ingredient => {
+        return ingredient.name;
+      });
+      this.trie.populate(nameArray);
+      this.setState({
+        allIngredients: nextProps.ingredientsObject.ingredients
+      });
+    }
   }
 
   handleChange(event) {
     const { name, value } = event.target;
-    
+
     this.setState({ [name]: value });
+    this.suggestIngredient(value);
   }
 
-  async submitForm(event) {
-    event.preventDefault();
-    const { setIngredients } = this.props;
-    const { userInput } = this.state;
-    const searchReasult = await searchForIngredient(userInput);
-    setIngredients(searchReasult);
+  // async submitForm(event) {
+  //   event.preventDefault();
+  //   const { setIngredients } = this.props;
+  //   const { userInput } = this.state;
+  //   const searchReasult = await searchForIngredient(userInput);
+  //   setIngredients(searchReasult);
+  // }
+
+  suggestIngredient(input) {
+    const suggestedIngredients = this.trie.suggest(input);
+
+    this.setState({
+      suggestedIngredients
+    });
   }
 
   render() {
     return (
       <div className="form-container">
         <form onSubmit={event => this.submitForm(event)}>
+
           <input
+            autoComplete="off"
+            autoFocus
             name="userInput"
             type="text"
             onChange={event => this.handleChange(event)}
             placeholder="enter ingredient"
+            list="drop-down"
           />
+
+          <datalist id="drop-down">
+            {this.state.suggestedIngredients
+              .map(ingredient => {
+                return <option value={ingredient} key={ingredient} />;
+              })
+              .slice(0, 5)}
+          </datalist>
+
           <button onClick={event => this.submitForm(event)}>search</button>
         </form>
       </div>
@@ -44,12 +86,12 @@ export class MainForm extends Component {
   }
 }
 
-// export const mapStateToProps = state => ({
-
-// });
+export const mapStateToProps = state => ({
+  ingredientsObject: state.ingredients
+});
 
 export const mapDispatchToProps = dispatch => ({
   setIngredients: ingredient => dispatch(setIngredients(ingredient))
 });
 
-export default connect(null, mapDispatchToProps)(MainForm);
+export default connect(mapStateToProps, mapDispatchToProps)(MainForm);
