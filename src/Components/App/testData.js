19312b081419147bf31a3d5380a984855526ea21
Spencer Herms <slherms@gmail.com>
2018-02-21 13:43:48 -0700

    lowercases cleanData names

--- src/Components/App/testData.js
+++ src/Components/App/testData.js
@@ -149,25 +149,25 @@ export const allIngredients = [
 ];
 
 export const cleanData = [
-  {
-    description:
-      'The essence of 1724 Tonic Water has its origins in the Andes, on the mythical Inca Trail. This is not by chance. This is where quinine was discovered. There, this ingredient gains the authenticity of a product with roots in the region that go back hundreds of years. At 1724 metres above sea level. Not a metre above or below.',
-    id: 5902,
-    image: 'https://pantry.foodpairing.com/apps/4578_240_0',
-    name: '1724 Tonic'
-  },
-  {
-    description:
-      'The rind of the à la Chimay Bleue is regularly washed with the Chimay Blue Cap during his stay in the aging cellars of the monestarey. Pronounced flavor with a slight bitterness of the hops.',
-    id: 5440,
-    image: 'https://pantry.foodpairing.com/apps/1772_240_0',
-    name: 'A la Chimay Bleue'
-  },
-  {
-    description:
-      'The à la Chimay Rouge is a round, smooth cheese with creamy texture. Its natural rind is washed with Chimay Red Cap. Fruity flavour with slight bitterness.',
-    id: 5443,
-    image: 'https://pantry.foodpairing.com/apps/1773_240_0',
-    name: 'A la Chimay Rouge'
-  }
+    {
+      id: 5902,
+      name: '1724 tonic',
+      image: 'https://pantry.foodpairing.com/apps/4578_240_0',
+      description:
+        'The essence of 1724 Tonic Water has its origins in the Andes, on the mythical Inca Trail. This is not by chance. This is where quinine was discovered. There, this ingredient gains the authenticity of a product with roots in the region that go back hundreds of years. At 1724 metres above sea level. Not a metre above or below.'
+    },
+    {
+      id: 5440,
+      name: 'a la chimay bleue',
+      image: 'https://pantry.foodpairing.com/apps/1772_240_0',
+      description:
+        'The rind of the à la Chimay Bleue is regularly washed with the Chimay Blue Cap during his stay in the aging cellars of the monestarey. Pronounced flavor with a slight bitterness of the hops.'
+    },
+    {
+      id: 5443,
+      name: 'a la chimay rouge',
+      image: 'https://pantry.foodpairing.com/apps/1773_240_0',
+      description:
+        'The à la Chimay Rouge is a round, smooth cheese with creamy texture. Its natural rind is washed with Chimay Red Cap. Fruity flavour with slight bitterness.'
+    }
 ];
