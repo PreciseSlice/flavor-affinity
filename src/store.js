c62369a3e0d84ce43ed2a58cceb87974f3db79c0
Spencer Herms <slherms@gmail.com>
2018-02-22 21:08:57 -0700

    cleaned up formatting

--- src/store.js
+++ src/store.js
@@ -2,4 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './Reducers/index';
 import thunk from 'redux-thunk';
 
-export default () => createStore(rootReducer, applyMiddleware(thunk));
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export default () => createStore(rootReducer, devTools, applyMiddleware(thunk));
