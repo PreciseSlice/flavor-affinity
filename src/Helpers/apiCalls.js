7c8c7f778685fc518623834818720833662bca3d
Spencer Herms <slherms@gmail.com>
2018-02-24 20:05:25 -0700

    pass name through to pairing object

--- src/Helpers/apiCalls.js
+++ src/Helpers/apiCalls.js
@@ -44,14 +44,14 @@ export const cleanAllIngredients = ingredientsData => {
   });
 };
 
-export const getParings = async id => {
+export const getParings = async (id, name) => {
   const pairings = await fetchFromApi(
     `https://api.foodpairing.com/ingredients/${id}/pairings`
   );
 
   const allPairings = cleanPairings(pairings);
 
-  return slicePairings(allPairings)
+  return slicePairings(allPairings, name);
 };
 
 export const cleanPairings = ingredientsData => {
@@ -59,20 +59,20 @@ export const cleanPairings = ingredientsData => {
     return {
       id: ingredient._links.ingredient.id,
       name: ingredient._links.ingredient.name.toLowerCase(),
-      image: ingredient._links.ingredient._links.image.size_240,
-    }
-  })
-}
+      image: ingredient._links.ingredient._links.image.size_240
+    };
+  });
+};
 
-export const slicePairings = allPairings => {
-  
+export const slicePairings = (allPairings, name) => {
   const topFive = allPairings.slice(0, 5);
   const middleFive = allPairings.slice(5, 10);
-  const finalFive = allPairings.slice(10, 15)
+  const finalFive = allPairings.slice(10, 15);
 
   return {
+    name,
     topFive,
     middleFive,
     finalFive
-  }
-} 
+  };
+};
